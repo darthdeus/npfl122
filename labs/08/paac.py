7a91364781bd0cf3fcef82cede38ea5df84c3efc
Jakub Arnold <darthdeus@gmail.com>
2019-02-11 17:07:36 +0100

    PAAC wip

--- labs/08/paac.py
+++ labs/08/paac.py
@@ -22,11 +22,17 @@ class Network:
             # - add a fully connected layer of size args.hidden_layer and ReLU activation
             # - add a fully connected layer with num_actions and no activation, computing `logits`
             # - compute `self.probabilities` as tf.nn.softmax of `logits`
+            hidden = tf.layers.dense(self.states, args.hidden_layer, activation=tf.nn.relu)
+            logits = tf.layers.dense(hidden, num_actions, activation=None)
+
+            self.probabilities = tf.nn.softmax(logits)
 
             # TODO(reinforce_with_baseline): Compute `self.values`, starting with self.states and
             # - add a fully connected layer of size args.hidden_layer and ReLU activation
             # - add a fully connected layer with 1 output and no activation
             # - modify the result to have shape `[batch_size]` (you can use for example `[:, 0]`)
+            hidden = tf.layers.dense(self.states, args.hidden_layer, activation=tf.nn.relu)
+            self.values = tf.layers.dense(hidden, 1, activation=None)[:, 0]
 
             # TODO: Compute `loss` as a sum of three losses:
             # - sparse softmax cross entropy of `self.actions` and `logits`,
@@ -34,6 +40,14 @@ class Network:
             # - negative value of the distribution entropy (use `entropy` method of
             #   `tf.distributions.Categorical`) weighted by `args.entropy_regularization`.
             # - mean square error of the `self.returns` and `self.values`
+            l1 = tf.losses.sparse_softmax_cross_entropy(self.actions, logits)
+
+            l2_entropy = tf.distributions.Categorical(probs=[self.probabilities]).entropy()
+            l2 = -args.entropy_regularization * l2_entropy
+
+            l3 = tf.losses.mean_squared_error(self.returns, self.values)
+
+            loss = l1 + l2 + l3
 
             global_step = tf.train.create_global_step()
             self.training = tf.train.AdamOptimizer(args.learning_rate).minimize(loss, global_step=global_step, name="training")
@@ -65,8 +79,8 @@ if __name__ == "__main__":
     parser.add_argument("--hidden_layer", default=100, type=int, help="Size of hidden layer.")
     parser.add_argument("--learning_rate", default=0.001, type=float, help="Learning rate.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-    parser.add_argument("--workers", default=1, type=int, help="Number of parallel workers.")
+    parser.add_argument("--threads", default=8, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--workers", default=8, type=int, help="Number of parallel workers.")
     args = parser.parse_args()
 
     # Create the environment
@@ -82,15 +96,49 @@ if __name__ == "__main__":
         # Training
         for _ in range(args.evaluate_each):
             # TODO: Choose actions using network.predict_actions
+            preds = network.predict_actions(states)
+
+            actions = [np.random.choice(env.actions, p=p) for p in preds]
+            # actions = preds.argmax(axis=1)
 
             # TODO: Perform steps by env.parallel_step
+            xxx = env.parallel_step(actions)
+
+            # dones lol
+            next_states, rewards, dones, _ = list(zip(*xxx))
+            next_values = network.predict_values(next_states)
+            # next_states, reward, done, _
 
             # TODO: Compute return estimates by
             # - extracting next_states from steps
             # - computing value function approximation in next_states
             # - estimating returns by reward + (0 if done else args.gamma * next_state_value)
+            returns = []
+            for next_value, reward, action, done in zip(next_values, rewards, actions, dones):
+                if done:
+                    ret = reward
+                else:
+                    ret = reward  + args.gamma * next_value
 
+                returns.append(ret)
+                #     next_state_value = np.zeros_like(next_states, dtype=np.float32)
+                # else:
+                #     next_state_value = network.predict_values(next_states)
+
+                # TODO: next_state_value only for action?
+                # returns.append(reward + args.gamma * next_state_value[action])
+
+            # returns = reward + args.gamma * next_state_value
+            states = np.array(states,   dtype=np.float32)
+            actions = np.array(actions, dtype=np.int32)
+            returns = np.array(returns, dtype=np.float32)
+
+            # __import__('ipdb').set_trace()
+
+            # print(states.shape, actions.shape, returns.shape)
+            # __import__('ipdb').set_trace()
             # TODO: Train network using current states, chosen actions and estimated returns
+            network.train(states, actions, returns)
 
             states = next_states
 
