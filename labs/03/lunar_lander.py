d2dcc659e20e12e93777f38593eed9f90c70640f
Jakub Arnold <darthdeus@gmail.com>
2019-02-13 23:13:42 +0100

    Add lunar_lander & q_learning + old

--- labs/03/lunar_lander.py
+++ labs/03/lunar_lander.py
@@ -1,5 +1,54 @@
+# #!/usr/bin/env python3
+# import numpy as np
+#
+# import lunar_lander_evaluator
+#
+# if __name__ == "__main__":
+#     # Fix random seed
+#     np.random.seed(42)
+#
+#     # Parse arguments
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
+#     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+#
+#     parser.add_argument("--alpha", default=None, type=float, help="Learning rate.")
+#     parser.add_argument("--alpha_final", default=None, type=float, help="Final learning rate.")
+#     parser.add_argument("--epsilon", default=None, type=float, help="Exploration factor.")
+#     parser.add_argument("--epsilon_final", default=None, type=float, help="Final exploration factor.")
+#     parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
+#     args = parser.parse_args()
+#
+#     # Create the environment
+#     env = lunar_lander_evaluator.environment()
+#
+#     # The environment has env.states states and env.actions actions.
+#
+#     # TODO: Implement a suitable RL algorithm.
+#     #
+#     # The overall structure of the code follows.
+#     while training:
+#
+#         # To generate expert trajectory, you can use
+#         # state, trajectory = env.expert_trajectory()
+#
+#         # Perform a training episode
+#         state, done = env.reset(), False
+#         while not done:
+#             if args.render_each and env.episode and env.episode % args.render_each == 0:
+#                 env.render()
+#
+#             next_state, reward, done, _ = env.step(action)
+#
+#     # Perform last 100 evaluation episodes
+
 #!/usr/bin/env python3
+import glob
 import numpy as np
+import random
+import tensorflow as tf
+tf.enable_eager_execution()
 
 import lunar_lander_evaluator
 
@@ -10,73 +59,142 @@ if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--episodes", default=10000, type=int, help="Training episodes.")
-    parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+    parser.add_argument("--episodes", default=2000, type=int, help="Training episodes.")
+    parser.add_argument("--render_each", default=100, type=int, help="Render some episodes.")
 
-    parser.add_argument("--alpha", default=0.1, type=float, help="Learning rate.")
+    parser.add_argument("--alpha", default=0.05, type=float, help="Learning rate.")
     parser.add_argument("--alpha_final", default=None, type=float, help="Final learning rate.")
-    parser.add_argument("--epsilon", default=0.06, type=float, help="Exploration factor.")
-    parser.add_argument("--epsilon_final", default=0.06, type=float, help="Final exploration factor.")
+    parser.add_argument("--epsilon", default=0.4, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon_final", default=0.01, type=float, help="Final exploration factor.")
     parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
-
-    # parser.add_argument("--alpha", default=0.02, type=float, help="Learning rate.")
-    # parser.add_argument("--alpha_final", default=None, type=float, help="Final learning rate.")
-    # parser.add_argument("--epsilon", default=0.2, type=float, help="Exploration factor.")
-    # parser.add_argument("--epsilon_final", default=0.5, type=float, help="Final exploration factor.")
-    # parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
     args = parser.parse_args()
 
     # Create the environment
     env = lunar_lander_evaluator.environment()
 
-    # The environment has env.states states and env.actions actions.
-
-    eps_update = (args.epsilon - args.epsilon_final) / args.episodes
-
     # TODO: Implement Q-learning RL algorithm.
-    epsilon = args.epsilon
-    # Q = np.zeros((env.states, env.actions))
-    Q = 5*np.ones((env.states, env.actions))
-
-    Q = np.load("Q3.dat.npy")
-
+    #
     # The overall structure of the code follows.
-    for i in range(args.episodes):
+    training = True
+
+    Q = np.random.random([env.states, env.actions])
+    C = np.zeros_like(Q)
+    # Q.fill(200)
+
+    eps_diff = (args.epsilon_final - args.epsilon) / float(args.episodes)
+    eps_curr = args.epsilon
+
+    iter = 0
+
+    trajs = []
+
+    def get_trajectory():
+        state, trajectory = env.expert_trajectory()
+
+        full_trajectory = []
+        for action, reward, next_state in trajectory:
+            full_trajectory.append((state, action, reward, next_state))
+            state = next_state
+
+        return full_trajectory
+
+    # trajectories = [get_trajectory() for _ in range(10000)]
+    # np.save("trajectories.npy", np.array(trajectories))
+    # print("generated trajectories")
+    trajectories = np.load("trajectories.npy")
+    print("loaded trajectories")
+
+    maxname = max([int(name.replace("logs/", "")) for name in glob.glob("logs/*")])
+
+    logdir = "./logs/{}".format(maxname + 1)
+    print("Logging into {}".format(logdir))
+
+    global_step = tf.train.get_or_create_global_step()
+    writer = tf.contrib.summary.create_file_writer(logdir)
+    writer.set_as_default()
+
+    def imitation():
+        # state, trajectory = env.expert_trajectory()
+        # trajectory = get_trajectory()
+        # trajs.extend(trajectory)
+
+        # ITER_PER_TRAJECTORY = 1
+
+        # for _ in range(ITER_PER_TRAJECTORY):
+        for trajectory in trajectories:
+            rev = list(reversed(trajectory))
+
+            G = 0.0
+
+            for state, action, reward, next_state in trajectory:
+                G = args.gamma * G + reward
+
+                C[state, action] += 1
+                Q[state, action] += (G - Q[state, action])/C[state, action]
+
+                # Q[state, action] = G
+
+                # G = args.gamma * G
+
+                    # Q[state, action] += 1.0 * \
+                    #         (reward + 0.98*np.max(Q[next_state]) - Q[state, action])
+
+        print("{}\tepsilon: {}\t{}\t{}".format(iter, eps_curr, Q.mean(), len(trajs)))
+
+        state, done = env.reset(), False
+        while not done:
+            env.render()
+
+            action = np.argmax(Q[state]).item()
+            state, _, done, _ = env.step(action)
+
+    print("Imitation START")
+
+    global_step.assign_add(1)
+    with tf.contrib.summary.record_summaries_every_n_global_steps(1):
+        tf.contrib.summary.histogram("Q", Q.reshape(-1))
+
+    imitation()
+
+    global_step.assign_add(1)
+    with tf.contrib.summary.record_summaries_every_n_global_steps(1):
+        tf.contrib.summary.histogram("Q", Q.reshape(-1))
+
+    print("Imitation DONE, continuing with Q-learning")
+
+    while training:
+        global_step.assign_add(1)
+        with tf.contrib.summary.record_summaries_every_n_global_steps(1):
+            tf.contrib.summary.histogram("Q", Q.reshape(-1))
+
         # Perform a training episode
         state, done = env.reset(), False
-        if i % 100 == 0:
-            print(Q.mean())
-
         while not done:
             if args.render_each and env.episode and env.episode % args.render_each == 0:
                 env.render()
 
-            rand = np.random.rand()
-            if rand <= epsilon:
-                action = np.random.randint(0, env.actions)
+            if random.random() < eps_curr:
+                action = random.randint(0, env.actions - 1)
             else:
-                action = np.argmax(Q[state, :])
+                action = np.argmax(Q[state]).item()
 
             next_state, reward, done, _ = env.step(action)
 
-            Q[state, action] = Q[state, action] + args.alpha * (
-                    reward + args.gamma * np.max(Q[next_state, :]) - Q[state, action])
+            # max_qsa = np.max(Q[next_state]).item()
+            Q[state, action] += 1.0 * \
+                    (reward + args.gamma*np.max(Q[next_state]) - Q[state, action])
+
             state = next_state
 
-        if i < args.episodes:
-            epsilon = epsilon - eps_update
+        eps_curr = max(0.05, eps_curr + eps_diff)
+        if env.episode % args.render_each == 0:
+            print("epsilon: {}".format(eps_curr))
 
-    np.save("Q4.dat", Q)
-
-    env.reset(start_evaluate=True)
-
-    for _ in range(100):
-
-        # Perform an evaluation episode
-        state, done = env.reset(), False
-        while not done:
-            if args.render_each and env.episode and env.episode % args.render_each == 0:
+            state, done = env.reset(), False
+            while not done:
                 env.render()
 
-            action = np.argmax(Q[state, :])
-            state, reward, done, _ = env.step(action)
+                action = np.argmax(Q[state]).item()
+                state, _, done, _ = env.step(action)
+
+    # Perform last 100 evaluation episodes
--- labs/03/q_learning.py
+++ labs/03/q_learning.py
@@ -1,85 +1,69 @@
 #!/usr/bin/env python3
-# in team with:
-# Jakub Arnold 2894e3d5-bd76-11e7-a937-00505601122b
-# Petra Doubravová 7ac09119-b96f-11e7-a937-00505601122b
 import numpy as np
+import random
 
 import mountain_car_evaluator
 
-
-def epsilon_greedy_action(state):
-    rand = np.random.rand()
-    if rand <= epsilon:
-        return np.random.randint(0, env.actions)
-    else:
-        return np.argmax(Q[state, :])
-
-
 if __name__ == "__main__":
     # Fix random seed
-    np.random.seed(2)
+    np.random.seed(42)
 
     # Parse arguments
     import argparse
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--episodes", default=10000, type=int, help="Training episodes.")
-    parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+    parser.add_argument("--episodes", default=2000, type=int, help="Training episodes.")
+    parser.add_argument("--render_each", default=250, type=int, help="Render some episodes.")
 
-    parser.add_argument("--alpha", default=0.1, type=float, help="Learning rate.")
+    parser.add_argument("--alpha", default=0.05, type=float, help="Learning rate.")
     parser.add_argument("--alpha_final", default=None, type=float, help="Final learning rate.")
-    parser.add_argument("--epsilon", default=0.45, type=float, help="Exploration factor.")
-    parser.add_argument("--epsilon_final", default=0.06, type=float, help="Final exploration factor.")
+    parser.add_argument("--epsilon", default=0.4, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon_final", default=0.01, type=float, help="Final exploration factor.")
     parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
     args = parser.parse_args()
 
     # Create the environment
     env = mountain_car_evaluator.environment()
 
-    mean = np.zeros(100)
-    i = 0
+    # TODO: Implement Q-learning RL algorithm.
+    #
+    # The overall structure of the code follows.
     training = True
 
-    eps_step_update = (args.epsilon - args.epsilon_final) / args.episodes
+    Q = np.random.random([env.states, env.actions])
+    Q.fill(-300)
 
-    # TODO: Implement Q-learning RL algorithm.
-    epsilon = args.epsilon
-    Q = np.zeros((env.states, env.actions))
+    eps_diff = (args.epsilon_final - args.epsilon) / float(args.episodes)
+    eps_curr = args.epsilon
 
     while training:
-        if i > args.episodes:
-            break
-
+        # Perform a training episode
         state, done = env.reset(), False
-        avg_reward = 0
-
         while not done:
             if args.render_each and env.episode and env.episode % args.render_each == 0:
                 env.render()
 
-            action = epsilon_greedy_action(state)
+            if random.random() < eps_curr:
+                action = random.randint(0, env.actions - 1)
+            else:
+                action = np.argmax(Q[state]).item()
+
             next_state, reward, done, _ = env.step(action)
-            avg_reward += reward
-            Q[state, action] = Q[state, action] + args.alpha * (
-                        reward + args.gamma * np.max(Q[next_state, :]) - Q[state, action])
+
+            max_qsa = np.max(Q[next_state]).item()
+            # TODO: ???????????/
+            Q[state, action] += args.alpha * (reward + args.gamma*(max_qsa - Q[state, action]))
+
             state = next_state
 
-        i += 1
-        mean[i % 100] = avg_reward
-        if i >= 100:
+        eps_curr = max(0.05, eps_curr + eps_diff)
+        if env.episode % args.render_each == 0:
+            print("epsilon: {}".format(eps_curr))
 
-            avg = np.average(mean)
-            if avg > -50:
-                training = False
-        if i < args.episodes:
-            epsilon = epsilon - eps_step_update
+            state, done = env.reset(), False
+            while not done:
+                env.render()
+
+                action = np.argmax(Q[state]).item()
+                state, _, done, _ = env.step(action)
 
     # Perform last 100 evaluation episodes
-    while True:
-        state, done = env.reset(True), False
-        while not done:
-            if args.render_each and env.episode and env.episode % args.render_each == 0:
-                env.render()
-            action = np.argmax(Q[state, :])
-            next_state, reward, done, _ = env.step(action)
-            state = next_state
