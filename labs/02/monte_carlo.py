21f240bdf18851cdfe51da12b2d5fa161a477369
Jakub Arnold <darthdeus@gmail.com>
2019-04-22 19:28:15 +0200

    Fix MC

--- labs/02/monte_carlo.py
+++ labs/02/monte_carlo.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     parser.add_argument("--episodes", default=5000, type=int, help="Training episodes.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 
-    parser.add_argument("--epsilon", default=0.1, type=float, help="Exploration factor.")
-    parser.add_argument("--epsilon_final", default=0.01, type=float, help="Final exploration factor.")
+    parser.add_argument("--epsilon", default=0.2, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon_final", default=0.1, type=float, help="Final exploration factor.")
     parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
     args = parser.parse_args()
 
@@ -27,26 +27,19 @@ if __name__ == "__main__":
     env = cart_pole_evaluator.environment()
 
     # TODO: Implement Monte-Carlo RL algorithm.
-    #
-    # The overall structure of the code follows.
     training = True
 
-    returns = defaultdict(lambda: [])
     Q = np.zeros([env.states, env.actions], dtype=np.float32)
-    Q.fill(50.)
+    Q.fill(500)
+    # Q.fill(1 / args.epsilon)
 
     C = np.zeros([env.states, env.actions], dtype=np.float32)
-
-    policy = np.random.randint(0, env.actions, env.states, dtype=np.int32)
 
     eps_diff = (args.epsilon_final - args.epsilon) / float(args.episodes)
     eps_curr = args.epsilon
 
-    episode_num = 0
-
     while training:
-        episode_num += 1
-        G = 0.0
+        trajectory = []
 
         # Perform a training episode
         state, done = env.reset(), False
@@ -57,41 +50,44 @@ if __name__ == "__main__":
             if random.random() < eps_curr:
                 action = random.randint(0, env.actions - 1)
             else:
-                action = policy[state].item()
+                action = np.argmax(Q[state]).item()
 
             next_state, reward, done, _ = env.step(action)
 
-            # print("G: {}, action: {}, policy: {}".format(G, action, policy[:5]))
+            trajectory.append([state, action, reward])
 
+            state = next_state
+
+        G = 0.0
+
+        for state, action, reward in reversed(trajectory):
             G = args.gamma * G + reward
-
-            C[state, action] += 1
             # returns[(state, action)].append(G)
             # Q[state, action] = np.mean(returns[(state, action)]).item()
-            Q[state, action] += (G - Q[state, action])/C[state, action]
 
-            policy[state] = np.argmax(Q[state, :]).item()
+            C[state, action] += 1
+            Q[state, action] += (G - Q[state, action])/C[state, action]
 
             state = next_state
 
         eps_curr += eps_diff
 
-        if args.render_each and episode_num % args.render_each == 0:
+        if args.render_each and env.episode % args.render_each == 0:
             print(f"eps curr: {eps_curr}")
 
             # Evaluation episode
             state, done = env.reset(), False
             while not done:
                 env.render()
-                action = policy[state].item()
+                action = np.argmax(Q[state]).item()
                 state, _, done, _ = env.step(action)
 
-        if episode_num > args.episodes:
+        if env.episode > args.episodes:
             break
 
     # Perform last 100 evaluation episodes
     while True:
         state, done = env.reset(True), False
         while not done:
-            action = policy[state].item()
+            action = np.argmax(Q[state]).item()
             state, _, done, _ = env.step(action)
